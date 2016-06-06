3e3bffc82d6beebe70bc57baef7c5816f9ff2fc1
Kyle Economou <kyle.economou@gmail.com>
2016-06-07 00:32:02 +0200

    got the titles, need to put these two chunks together into one beautiful chunk. progress is commented out

--- coyote/app/controllers/home_controller.rb
+++ coyote/app/controllers/home_controller.rb
@@ -4,15 +4,28 @@ class HomeController < ApplicationController
   	require 'open-uri'
     require 'nokogiri'
 
-    # .cellMainLink
+    page_object = Nokogiri::HTML(open("https://kat.cr/usearch/game%20of%20thrones/"))
 
-    page_object = Nokogiri::HTML(open("https://kat.cr/usearch/game/"))
-
-    html_data = page_object.xpath("//a[@title='Torrent magnet link']/@href").map do |n|
+    magnet_links = page_object.xpath("//a[@title='Torrent magnet link']/@href").map do |n|
     	n.text
     end
-    	
-    @sup = html_data
+
+    @sup = magnet_links
+
+    torrent_titles = page_object.xpath("//a[@class='cellMainLink']").map do |n|
+    	n.text
+    end
+
+    @sup = magnet_links
+    @chillin = torrent_titles
+    
+  #   entire_result = page_object.xpath("//tr").each do |n|
+		# doc.css("NewsResult").map{|nr| [nr.at('Title'),nr.at('Snippet'),nr.at('Url'),nr.at('Source')].map(&:text)}
+  #   end
+
+  #   @result = entire_result
+  #   # @torrent_title = entire_result.torrent_title
+  #   # @magnet_link = entire_result.magnet_link
 
   end
 end
